1bdee0ab11194e7bd76976bc9c6680e1af2344fb
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-21 19:43:58 +0530

    variables code

--- src/main/java/com/variables/Variables.java
+++ src/main/java/com/variables/Variables.java
@@ -4,8 +4,9 @@ public class Variables {
     public static void main(String[] args) {
         int x = 5;
         System.out.println("x = " + x);
-        System.out.println("x++ "+x++);
+        System.out.println(x++);
         System.out.println("x = " + x);
+        System.out.println(--x);
 
     }
 }
