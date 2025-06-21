aaf0833e3c32de75b8fadfcab4cc826b4eee9157
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-21 19:27:14 +0530

    initial commit

---
+++ src/main/java/com/variables/Variables.java
@@ -0,0 +1,7 @@
+package com.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.tanmoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
