32d832669c30aca7aa99fdb0316fd8f3cd751c18
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-21 19:50:16 +0530

    variables code

---
+++ src/main/java/com/variables/Doubles.java
@@ -0,0 +1,12 @@
+package com.variables;
+
+public class Doubles {
+    public static void main(String[] args) {
+        double d1 = 5.0;
+        System.out.println("d1 = " + d1);
+
+        System.out.println(5.0/2);
+        System.out.println(5/2.0);
+        System.out.println(5.0/2.0);
+    }
+}
