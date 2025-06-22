472a49b21f621c32ad0b1be4b2f2bc9d690dd2de
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-22 17:57:13 +0530

    java if-else

--- src/main/java/com/chars/Chars.java
+++ src/main/java/com/chars/Chars.java
@@ -4,5 +4,7 @@ public class Chars {
     public static void main(String[] args) {
         char c = 'a';
         System.out.println(c);
+        int i = (int) c;
+        System.out.println("i = " + i);
     }
 }
---
+++ src/main/java/com/input/Calc.java
@@ -0,0 +1,8 @@
+package com.input;
+
+public class Calc {
+    public static void main(String[] args) {
+        double d = 7/22*(3.14*2)*3/5;
+        System.out.println("d = " + d);
+    }
+}
