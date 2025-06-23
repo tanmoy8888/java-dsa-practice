894c6676fa65e4719c40108be442ae4df5711b95
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-23 22:30:55 +0530

    java if-else

---
+++ src/main/java/com/if_else/MaxAmongThree.java
@@ -0,0 +1,22 @@
+package com.if_else;
+
+import java.util.Scanner;
+
+/**
+ * Take three positive integers and found max
+ */
+public class MaxAmongThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a > b && a > c){
+            System.out.println("a is maximum");
+        } else if (b > a && b > c) {
+            System.out.println("b is maximum");
+        } else {
+            System.out.println("c is maximum");
+        }
+    }
+}
