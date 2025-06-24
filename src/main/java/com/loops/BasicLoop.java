a9843990c25c417d0e062a89c2d76eb85697869a
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-24 19:38:01 +0530

    java loop

--- src/main/java/com/loops/BasicLoop.java
+++ src/main/java/com/loops/BasicLoop.java
@@ -1,4 +1,10 @@
 package com.loops;
 
 public class BasicLoop {
+    public static void main(String[] args) {
+        for (int i =0 ;i <= 5;i++){
+            System.out.println("Hello");
+            System.out.println("World !!");
+        }
+    }
 }
