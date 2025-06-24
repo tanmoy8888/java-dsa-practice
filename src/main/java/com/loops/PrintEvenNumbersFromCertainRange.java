423c9ba0ce9f86d0ee6eeac4a0b967fc22ffee10
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-24 20:12:24 +0530

    java loop

---
+++ src/main/java/com/loops/PrintEvenNumbersFromCertainRange.java
@@ -0,0 +1,15 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class PrintEvenNumbersFromCertainRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <=N ; i++) {
+            if(i % 2 ==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
