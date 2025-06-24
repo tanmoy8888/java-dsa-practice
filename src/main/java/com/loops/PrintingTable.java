cd20bda5c068c29ed6291a6aa0fc89931a94ba86
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-24 20:20:19 +0530

    java loop

--- src/main/java/com/loops/PrintingTable.java
+++ src/main/java/com/loops/PrintingTable.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class PrintingTable {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         for (int i = 1; i <=10 ; i++) {
             System.out.println("Table of "+number+" , "+i+" is "+i*number);
+        }*/
+
+        // Another approach 
+        for (int i = 19; i <= 190; i++) {
+            if(i % 19 ==0){
+                System.out.println(i);
+            }
         }
     }
 }
