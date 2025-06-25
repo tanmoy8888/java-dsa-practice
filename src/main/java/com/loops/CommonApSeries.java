96586a0a1c08ec425dfef459bb675734769aa07b
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-25 20:13:15 +0530

    java loop

--- src/main/java/com/loops/CommonApSeries.java
+++ src/main/java/com/loops/CommonApSeries.java
@@ -2,6 +2,11 @@ package com.loops;
 
 import java.util.Scanner;
 
+/**
+ *  This is a common generic AP solution , we just need to change initial and increment variable value,
+ *  and it will print AP series of N number.
+ */
+
 public class CommonApSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
