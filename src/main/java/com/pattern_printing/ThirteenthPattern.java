6eceb251a455d1a4e29dac2240bfbbfc0d8cfaa9
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 18:36:29 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/ThirteenthPattern.java
@@ -0,0 +1,31 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ *
+ *  Print this below pattern
+ *    m = 4
+ *    n = 6
+ *      * * * * * *
+ *      *         *
+ *      *         *
+ *      * * * * * *
+ *
+ */
+public class ThirteenthPattern {
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 6;
+        for (int i = 1; i <=m ; i++) {
+            for (int j = 1; j <=n ; j++) {
+               if(i == 1 || i == 4 || j == 1 || j ==6){
+                   System.out.print("*"+" ");
+               }else {
+                   System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+    }
+}
