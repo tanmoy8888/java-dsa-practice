1a8983328aa33dd68b021cdd1b74a2a436d0c78c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 15:51:00 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/FifthPattern.java
@@ -0,0 +1,16 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+public class FifthPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 0; i <number ; i++) {
+            for (int j = 0; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
