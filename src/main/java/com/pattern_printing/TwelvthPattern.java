852a2bae80705602fd6aa00d2cb0f0aa0b1c3e94
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 18:28:25 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/TwelvthPattern.java
@@ -0,0 +1,35 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ * Pattern
+ *  I/P = > Inout N will always be an odd integer
+ *  O/P => For N = 5
+ *
+ *                  *
+ *                  *
+ *              * * * * *
+ *                  *
+ *                  *
+ * Hint : Find middle element , formula to find a middle element of a odd number is (N/2 +1)
+ *
+ */
+public class TwelvthPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int middle = number/2+1;
+        for (int i = 1; i <=number ; i++) {
+            for (int j = 1; j <=number ; j++) {
+                if(i==middle || j == middle) {
+                    System.out.print("*" + " ");
+                }else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+            }
+        }
+}
