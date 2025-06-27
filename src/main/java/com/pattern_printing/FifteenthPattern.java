208d9038774e1a9632dd1825ee1b665b89be5fb2
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 20:18:56 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/FifteenthPattern.java
@@ -0,0 +1,48 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ * Print this below pattern
+ *
+ * I/P => N = 4
+ * O/P =>
+ *            1
+ *          1 2
+ *        1 2 3
+ *      1 2 3 4
+ */
+public class FifteenthPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        /*for (int i = 1; i <=number ; i++) {
+            for (int j = 1; j <= number; j++) {
+                if(j <= number-i){
+                    System.out.print("  ");
+                }else {
+                    System.out.print(j+" ");
+                }
+            }
+            System.out.println();
+        }
+        O/P =>
+
+       I/P => 4
+      4
+    3 4
+  2 3 4
+1 2 3 4
+        */
+        for (int i = 1; i <=number ; i++) {
+            for (int j = 1; j <= number-i; j++) {
+                System.out.print(" "+" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
