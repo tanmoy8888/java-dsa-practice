f488f09c0ec800773e3fda19f738168e05375cf2
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 12:53:53 +0530

    java loop

---
+++ src/main/java/com/loops/Factorial.java
@@ -0,0 +1,16 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int fact = 1;
+        while (number != 0){
+            fact = number*fact;
+            number--;
+        }
+        System.out.println("fact = " + fact);
+    }
+}
