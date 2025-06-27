9c84e1c0149aa8b18ab49acde622cdefea80246c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 20:49:21 +0530

    java loop , pattern

--- src/main/java/com/pattern_printing/SixteenthPattern.java
+++ src/main/java/com/pattern_printing/SixteenthPattern.java
@@ -22,7 +22,7 @@ public class SixteenthPattern {
                 }
             }
             for (int j = 1; j <=2*i-1 ; j++) {
-                System.out.print("*"+ "  ");
+                System.out.print("*"+ " ");
             }
             System.out.println();
         }
