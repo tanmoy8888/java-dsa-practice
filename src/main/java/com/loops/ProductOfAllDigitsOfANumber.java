bb1b5fd2e3d74e8b431f3af3e6b2f6b253c4ce84
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 11:58:29 +0530

    java loop

---
+++ src/main/java/com/loops/ProductOfAllDigitsOfANumber.java
@@ -0,0 +1,21 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class ProductOfAllDigitsOfANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int product = 1;
+        int reminder = 0;
+        while (number > 0){
+            reminder = number%10;
+            if(number <0){
+                product *=product*number;
+            }
+            product *= reminder;
+            number = number/10;
+        }
+        System.out.println("product = " + product);
+    }
+}
--- src/main/java/practice/loops/SumOfDigit.java
+++ src/main/java/practice/loops/SumOfDigit.java
@@ -6,15 +6,15 @@ public class SumOfDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int reminder =0;
+        int reminder = 0;
         int sum =0;
         while (number>0){
             reminder = number%10;
             if(number <10){
-                sum = sum + number;
+                sum += number;
                 break;
             }
-            sum = sum+ reminder;
+            sum+= reminder;
             number= number/10;
         }
         System.out.println("sum = " + sum);
