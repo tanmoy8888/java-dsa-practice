7d77d4a15ea1a10d1197a1bb21ba7f54fc6dbb4e
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 11:51:52 +0530

    java loop

---
+++ src/main/java/practice/loops/SumOfDigit.java
@@ -0,0 +1,22 @@
+package practice.loops;
+
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reminder =0;
+        int sum =0;
+        while (number>0){
+            reminder = number%10;
+            if(number <10){
+                sum = sum + number;
+                break;
+            }
+            sum = sum+ reminder;
+            number= number/10;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
