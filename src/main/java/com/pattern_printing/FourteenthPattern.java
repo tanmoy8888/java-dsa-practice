f2c29984e1803ffcebf74d5fef87a0575e77709f
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 19:40:54 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/FourteenthPattern.java
@@ -0,0 +1,33 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ * Print this below pattern
+ *
+ *                      *
+ *                    * *
+ *                  * * *
+ *                * * * *
+ *
+ * hint : (i+j) < n sout("*")
+ *
+ * Important : Don't forget to add an actual space like in line no 24 two spaces should be there ("  ")
+ */
+public class FourteenthPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number ; j++) {
+                if(j <= number-i){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("*"+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
