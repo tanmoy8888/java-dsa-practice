a423d229c308a8d19a393a34f54b13f2ab26bdb6
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 13:34:07 +0530

    java loop

---
+++ src/main/java/com/loops/ASCIIValuesOfAlphabets.java
@@ -0,0 +1,15 @@
+package com.loops;
+
+public class ASCIIValuesOfAlphabets {
+    public static void main(String[] args) {
+      int i = 65;
+      while (i > 0){
+          char c = (char) i;
+          System.out.println("ASCII value of :: " + c +", is :: "+i);
+          i++;
+          if(i >= (65+26)){
+              break;
+          }
+      }
+    }
+}
