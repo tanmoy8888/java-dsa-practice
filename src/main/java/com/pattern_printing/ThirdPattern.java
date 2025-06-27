755ff978a0704ac7d4fc976938ffd93cb59c6c42
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 15:33:57 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/ThirdPattern.java
@@ -0,0 +1,16 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+public class ThirdPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 1; i <=number ; i++) {
+            for (int j = 1; j <= number; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
