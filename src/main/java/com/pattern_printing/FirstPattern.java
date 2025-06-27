1dc5f6d3ad14496e681daf99db02cd5b31bd43cd
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 15:23:05 +0530

    java loop

--- src/main/java/com/pattern_printing/FirstPattern.java
+++ src/main/java/com/pattern_printing/FirstPattern.java
@@ -4,7 +4,7 @@ public class FirstPattern {
     public static void main(String[] args) {
         for (int i = 1; i <=3 ; i++) {
             for (int j = 1; j <=5; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
