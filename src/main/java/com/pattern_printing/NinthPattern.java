ad37d64c4a6e67faa52f1549af22a8098055e4a2
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 17:07:08 +0530

    java loop

--- src/main/java/com/pattern_printing/NinthPattern.java
+++ src/main/java/com/pattern_printing/NinthPattern.java
@@ -1,4 +1,31 @@
 package com.pattern_printing;
 
+import java.util.Scanner;
+
+/**
+ * Print this below pattern ,
+ * I/P => 4
+ *
+ * O/P =>
+ *  1
+ *  1 3
+ *  1 3 5
+ *  1 3 5 7
+ *
+ *  Hint : (2j-1)  // Check this !!
+ *  
+ */
 public class NinthPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 1; i<=number ; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((2*j-1)+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
