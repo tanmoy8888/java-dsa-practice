45738007a486e99bae290869a88ec69c2e8413fa
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 16:24:20 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/EighthPattern.java
@@ -0,0 +1,26 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ * Print this below pattern
+ *   I/P => 4
+ *   O/P =>
+ *
+ *     * * * *
+ *     * * *
+ *     * *
+ *     *
+ */
+public class EighthPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = number; i >=0 ; i--) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
