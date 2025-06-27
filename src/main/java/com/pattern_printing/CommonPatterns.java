9dd8dd9b7a35717bbd4d874f8b83450c61b8335b
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 21:09:59 +0530

    java loop , pattern

---
+++ src/main/java/com/pattern_printing/CommonPatterns.java
@@ -0,0 +1,43 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ *  I/P =>  N = 3
+ *  O/P =>
+ *                  * * *
+ *                    * *
+ *                      *
+ *
+ * I/P =>  N = 5
+ *  *  O/P =>
+ *  *           * * * * *
+ *  *             * * * *
+ *  *               * * *
+ *  *                 * *
+ *  *                   *
+ *  *
+ *
+ *
+ */
+public class CommonPatterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int nsp=number-1;
+        int nst =number;
+        for (int i = 1; i <=number ; i++) {
+            for (int j = 0; j <=nsp ; j++) {
+                    System.out.print(" "+" ");
+            }
+            for (int j = 1; j <=nst ; j++) {
+                System.out.print("*"+" ");
+            }
+            nsp+=1;
+            nst--;
+            System.out.println();
+        }
+
+
+    }
+}
