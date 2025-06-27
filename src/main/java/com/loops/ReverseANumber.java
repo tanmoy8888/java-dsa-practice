bef9c4d4d2c2c8cf07ebcea3d57b4b10ba8dd7fe
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 12:33:47 +0530

    java loop

---
+++ src/main/java/com/loops/ReverseANumber.java
@@ -0,0 +1,26 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reminder = 0;
+        int reverse = 0;
+
+        while (number > 0){
+            if(number < 10){
+                reverse = reverse+number;
+                break;
+            }
+            else {
+                reminder = number % 10;
+                reverse = (reverse + reminder) * 10;
+            }
+            number = number/10;
+        }
+        System.out.println("reverse = " + reverse);
+
+    }
+}
