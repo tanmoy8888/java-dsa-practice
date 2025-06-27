933c24e0f9f03954a4ce45c8982b33f05c103ab0
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-27 16:14:40 +0530

    java loop

---
+++ src/main/java/com/pattern_printing/SeventhPattern.java
@@ -0,0 +1,37 @@
+package com.pattern_printing;
+
+import java.util.Scanner;
+
+/**
+ * Print this below pattern
+ *
+ * I/P => N = 2
+ * O/P =>
+ *       1
+ *       A B
+ *
+ * I/P => N = 4
+ * O/P =>
+ *          1
+ *          A B
+ *          1 2 3
+ *          A B C D
+ *
+ */
+public class SeventhPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 1; i <=number ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                    if(i%2 ==0) {
+                        System.out.print((char) (j+64) + " ");
+                    }else {
+                        System.out.print(j+" ");
+                    }
+
+            }
+            System.out.println();
+        }
+    }
+}
