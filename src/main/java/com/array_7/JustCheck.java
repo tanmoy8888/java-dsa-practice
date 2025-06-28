9cb8d0cbcea010ef23ceff8c5b3ee48b48e15214
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-28 22:36:51 +0530

    java method

---
+++ src/main/java/com/array_7/JustCheck.java
@@ -0,0 +1,12 @@
+package com.array_7;
+
+import java.util.Arrays;
+
+public class JustCheck {
+    public static void main(String[] args) {
+        // can't put any capacity in the [] when we are using values in {} in comma separated manner
+
+        int [] arr = new int[]{1,2,3,4};
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+    }
+}
---
+++ src/main/java/practice/loops/leetcode/math/IntegerBreak.java
@@ -0,0 +1,41 @@
+package practice.loops.leetcode.math;
+
+import java.util.Scanner;
+
+/**
+ *  Leetcode :  343 : Integer Break
+ *
+ *   Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+ *
+ * Return the maximum product you can get.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 1
+ * Explanation: 2 = 1 + 1, 1 × 1 = 1.
+ * Example 2:
+ *
+ * Input: n = 10
+ * Output: 36
+ * Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= n <= 58
+ *
+ */
+public class IntegerBreak {
+   /* public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("integerBreak(number) = " + integerBreak(number));
+    }
+
+    public static int integerBreak(int n) {
+
+    }*/
+}
