47435d8a06220e8c3f271fe914db8aaf3fe2c9d7
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-28 21:38:11 +0530

    java method

---
+++ src/main/java/practice/loops/leetcode/math/PerfectNumber.java
@@ -0,0 +1,48 @@
+package practice.loops.leetcode.math;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+ *
+ * Given an integer n, return true if n is a perfect number, otherwise return false.
+ *
+ * Example 1:
+ *
+ * Input: num = 28
+ * Output: true
+ * Explanation: 28 = 1 + 2 + 4 + 7 + 14
+ * 1, 2, 4, 7, and 14 are all divisors of 28.
+ * Example 2:
+ *
+ * Input: num = 7
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= num <= 108
+ *
+ */
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("checkPerfectNumber(number) = " + checkPerfectNumber(number));
+    }
+    public static boolean checkPerfectNumber(int num) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < num; i++) {
+            if(num%i ==0){
+               list.add(i);
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum+=list.get(i);
+        }
+        return sum == num;
+    }
+}
