32f2e20b8d62ba4bcdea5996038360a50506dbf6
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-28 19:28:10 +0530

    java method

---
+++ src/main/java/com/methods_6/SwapNumbers.java
@@ -0,0 +1,20 @@
+package com.methods_6;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping two number ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
