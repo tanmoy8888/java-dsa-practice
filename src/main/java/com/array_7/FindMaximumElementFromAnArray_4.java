f8b32d958f85d60623af535e46997733042a850a
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-29 13:01:43 +0530

    java array

--- src/main/java/com/array_7/FindMaximumElementFromAnArray_4.java
+++ src/main/java/com/array_7/FindMaximumElementFromAnArray_4.java
@@ -2,8 +2,9 @@ package com.array_7;
 
 public class FindMaximumElementFromAnArray_4 {
     public static void main(String[] args) {
-        int [] arr = {2,6,8,10,15,-1,0,3};
-        int maximum =0;
+        //int [] arr = {2,6,8,10,15,-1,0,3};
+        int [] arr = {-10,-2,-5,-3,-6,-7,-4,-9,-1};
+        int maximum = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if(maximum < arr[i]){
                 maximum = arr[i];
