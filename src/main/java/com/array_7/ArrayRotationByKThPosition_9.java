81836f1d61be6f4814caf28aef6c3d17417247c3
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-29 20:10:16 +0530

    java array

---
+++ src/main/java/com/array_7/ArrayRotationByKThPosition_9.java
@@ -0,0 +1,40 @@
+package com.array_7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * ****************  TWO POINTER ************
+ * <p>
+ * Rotate an array of N by K steps where K will be non-negative
+ * Note : K can also be greater than N where N is the size of the array.
+ * I/P = > int [] arr ={2,3,5,7,11,13,17} , k =5
+ */
+public class ArrayRotationByKThPosition_9 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7, 11, 13, 17};
+        System.out.println("original array = " + Arrays.toString(arr));
+        int k = 5;
+        if (k > args.length) {
+            k = k % arr.length;
+        }
+        // First Rotate entire array
+        rotateArray(arr, 0, arr.length - 1);
+        // Rotate first part
+        rotateArray(arr, 0, (arr.length - k - 1));
+        // Rotate second part
+        rotateArray(arr, (arr.length - k), arr.length - 1);
+        System.out.println("After Rotate 2 = " + Arrays.toString(arr));
+    }
+
+    private static void rotateArray(int[] arr, int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
--- src/main/java/com/array_7/TwoPointer_8.java
+++ src/main/java/com/array_7/TwoPointer_8.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class TwoPointer_8 {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6,7,8};
+        int [] arr = {1,2,3,4,5,6,7,8,9};
         System.out.println("Before = " + Arrays.toString(arr));
         int i = 0;
         int j = arr.length-1;
