564735e4b344a54afd7f4059dc3d871c2ab3e90c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-29 19:26:25 +0530

    java array

--- src/main/java/com/array_7/TwoPointer_8.java
+++ src/main/java/com/array_7/TwoPointer_8.java
@@ -1,7 +1,26 @@
 package com.array_7;
 
+import java.util.Arrays;
+
+/**
+ *             **************  TWO POINTER ***************
+ *
+ *  Given an int [] ={1,2,3,4,5,6,7,8} , reverse it without using extra array.
+ */
 public class TwoPointer_8 {
     public static void main(String[] args) {
-        
+        int [] arr = {1,2,3,4,5,6,7,8};
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+        int i = 0;
+        int j = arr.length-1;
+        int temp = 0;
+        while (i<j){
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
     }
 }
