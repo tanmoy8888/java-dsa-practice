d523a0d15c10f1cc498a7f48c9be00322f650168
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-29 19:28:13 +0530

    java array

--- src/main/java/com/array_7/TwoPointer_8.java
+++ src/main/java/com/array_7/TwoPointer_8.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class TwoPointer_8 {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,6,7,8};
-        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+        System.out.println("Before = " + Arrays.toString(arr));
         int i = 0;
         int j = arr.length-1;
         int temp = 0;
@@ -21,6 +21,6 @@ public class TwoPointer_8 {
             i++;
             j--;
         }
-        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+        System.out.println("After swap = " + Arrays.toString(arr));
     }
 }
