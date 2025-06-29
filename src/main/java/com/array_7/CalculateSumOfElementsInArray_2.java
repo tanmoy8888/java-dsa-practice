7611ba44fc344b85aeaaa31bc025522d15cdcb94
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-29 12:43:08 +0530

    java array

---
+++ src/main/java/com/array_7/CalculateSumOfElementsInArray_2.java
@@ -0,0 +1,17 @@
+package com.array_7;
+
+import java.util.Scanner;
+
+public class CalculateSumOfElementsInArray_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int [] arr = new int[len];
+        int sum =0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i+1;
+            sum +=arr[i];
+        }
+        System.out.println("sum = " + sum);
+    }
+}
