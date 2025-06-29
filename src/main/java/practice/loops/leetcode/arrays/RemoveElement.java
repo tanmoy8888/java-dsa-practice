34bbdc0282714af66cf210b0fd5b56276c90ef09
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-30 00:52:29 +0530

    java array

--- src/main/java/practice/loops/leetcode/arrays/RemoveElement.java
+++ src/main/java/practice/loops/leetcode/arrays/RemoveElement.java
@@ -44,6 +44,13 @@ public class RemoveElement {
                 }
             }
         }
-        return end+1;
+        int count =0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == val){
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
