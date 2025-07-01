d0d67538ebd22f6cd4c0fd048d497a7532c3e749
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-01 16:26:21 +0530

    java array

--- src/main/java/practice/leetcode/arrays/PlusOne.java
+++ src/main/java/practice/leetcode/arrays/PlusOne.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class PlusOne {
     public static void main(String[] args) {
         //int [] digits = {1,2,3};
-        int [] digits = {9};
+        //int [] digits = {9};
+        int [] digits = {9,8,7,6,5,4,3,2,1,0};
         System.out.println("Input array = " + Arrays.toString(digits));
-        plusOne(digits);
+        System.out.println("Output array = " + Arrays.toString(plusOne(digits)));
+        ;
     }
 
     public static int[] plusOne(int[] digits) {
@@ -25,51 +27,34 @@ public class PlusOne {
                 }
             }
         }
-        // System.out.println("number = " + number);
-        // Increment it by one
         int incrementedNumber = number+1;
         int num = incrementedNumber;
-        // System.out.println("incremented number = " + incrementedNumber);
         if(incrementedNumber < 10){
             return new int[]{incrementedNumber};
         }else {
-
-            // count digits to initialize the array
-
             int count = 0;
             while(incrementedNumber > 0){
                 int reminder = incrementedNumber%10;
                 count++;
                 incrementedNumber = incrementedNumber/10;
             }
-            System.out.println("count = " + count);
             int [] ans = new int[count];
-           /* for (int i = 0; i < ans.length; i++) {
-
-            }*/
-            // System.out.println("incrementedNumber = " + incrementedNumber);
             int reminderNew =-1;
             int digit = -1;
             int index = ans.length-1;
             while (num!=0){
                 reminderNew = num%10;
                 if(reminderNew > 0){
-                    // System.out.println("number = " + num);
                     digit = reminderNew;
                     ans[index] = digit;
-                    // System.out.println("digit 1 = " + digit);
                 }
                 if(num < 10){
-                    //  digit = number;
-                    // System.out.println("digit 2 = " + digit);
                     break;
                 }
                 num = num/10;
                 index--;
             }
-          //  System.out.println("Output array = " + Arrays.toString(ans));
             return ans;
-
         }
     }
 }
