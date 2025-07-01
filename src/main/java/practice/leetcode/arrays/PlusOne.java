02ae1276ebd58c9c511fb5f951406a9d02e1453a
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-01 18:27:19 +0530

    java array

--- src/main/java/practice/leetcode/arrays/PlusOne.java
+++ src/main/java/practice/leetcode/arrays/PlusOne.java
@@ -1,58 +1,70 @@
 package practice.leetcode.arrays;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
         //int [] digits = {1,2,3};
         //int [] digits = {9};
-        int [] digits = {9,8,7,6,5,4,3,2,1,0};
+       // int [] digits = {9,8,7,6,5,4,3,2,1,0};
+        int [] digits= {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
         System.out.println("Input array = " + Arrays.toString(digits));
         System.out.println("Output array = " + Arrays.toString(plusOne(digits)));
         ;
     }
 
     public static int[] plusOne(int[] digits) {
-        long number = 1 ;
+        BigInteger number = BigInteger.ONE ;
+        System.out.println("digits.length = " + digits.length);
         if(digits.length == 1) {
-            number = (long) digits[0];
+            number = BigInteger.valueOf(digits[0]);
         }else {
             for (int i = 0; i < digits.length; i++) {
                 if (i == 0) {
-                    number = (long)digits[i] * 10 + (long)digits[i + 1];
+                    number = BigInteger.valueOf(digits[i]*10).add(BigInteger.valueOf(digits[i + 1]));
                 } else {
                     if (i < digits.length - 1) {
-                        number = number * 10 + (long)digits[i + 1];
+                        number = number.multiply(BigInteger.valueOf(10)).add (BigInteger.valueOf(digits[i + 1]));
                     }
                 }
             }
         }
-        long incrementedNumber = number+1;
-        long num = incrementedNumber;
-        if(incrementedNumber < 10){
-            int incrementedNumber1 = (int) incrementedNumber;
+        BigInteger incrementedNumber = number.add(BigInteger.ONE);
+        BigInteger num = incrementedNumber;
+        System.out.println("num = " + num);
+        if(!(incrementedNumber.compareTo(BigInteger.valueOf(10)) > 0)){
+            System.out.println("inside true....");
+            int incrementedNumber1 = (int) incrementedNumber.intValue();
             return new int[]{incrementedNumber1};
         }else {
             int count = 0;
-            while(incrementedNumber > 0){
-                long reminder = incrementedNumber%10;
+            System.out.println("inside false....");
+            while(incrementedNumber.compareTo(BigInteger.ZERO) > 0){
+                BigInteger reminder = incrementedNumber.mod(BigInteger.TEN);
                 count++;
-                incrementedNumber = incrementedNumber/10;
+                incrementedNumber = incrementedNumber.divide(BigInteger.valueOf(10));
             }
+            System.out.println("count = " + count);
             int [] ans = new int[count];
-            long reminderNew =-1;
-            long digit = -1;
+            BigInteger reminderNew;
+            int digit = -1;
             int index = ans.length-1;
-            while (num!=0){
-                reminderNew = num%10;
-                if(reminderNew > 0){
-                    digit = reminderNew;
-                    ans[index] = (int)digit;
+            while (num.compareTo(BigInteger.ZERO) >0){
+                reminderNew = num.mod(BigInteger.TEN);
+                if(reminderNew.compareTo(BigInteger.ZERO) > 0){
+                    digit = reminderNew.intValue();
+                    System.out.println("digit = " + digit);
+                    ans[index] = digit;
+                    System.out.println("ans[index] = " + ans[index]);
                 }
-                if(num < 10){
+                if(num.compareTo(BigInteger.TEN) < 0){
+                    System.out.println("inside break...");
+                    ans[index] = num.intValue();
                     break;
                 }
-                num = num/10;
+                num = num.divide(BigInteger.TEN);
+                System.out.println("num = " + num);
                 index--;
             }
             return ans;
