4cfe45230dcc5ec65e4adec7e4aebc3d72d202f1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-03 00:20:31 +0530

    java array

--- src/main/java/practice/arrays/CopyZerosAtTheEnd.java
+++ src/main/java/practice/arrays/CopyZerosAtTheEnd.java
@@ -3,6 +3,7 @@ package practice.arrays;
 import java.util.Arrays;
 
 // Move all the zeros to the end.
+
 public class CopyZerosAtTheEnd {
     public static void main(String[] args) {
         int[] arr = {4, 5, 0, 10, 0, 1, 0, 3, 7, 0, 0};
