0f01c5144e9d1621607e7be2cb551df045cc0fb4
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-03 20:54:25 +0530

    java array

--- src/main/java/practice/leetcode/arrays/MissingNumber.java
+++ src/main/java/practice/leetcode/arrays/MissingNumber.java
@@ -1,11 +1,10 @@
 package practice.leetcode.arrays;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Leetcode :  268. Missing Number
+ *  Leetcode :  268. Missing Number
  *
  * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
  *
@@ -41,6 +40,8 @@ import java.util.Map;
  *
  * n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
  */
+
+
 public class MissingNumber {
     public static void main(String[] args) {
         //int [] nums = {3,0,1};
