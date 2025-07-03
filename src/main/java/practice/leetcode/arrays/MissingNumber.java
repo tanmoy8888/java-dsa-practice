74cb3bbf742b95c4c01affc0b91dd328675a8881
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-03 20:40:58 +0530

    java array

---
+++ src/main/java/practice/leetcode/arrays/MissingNumber.java
@@ -0,0 +1,34 @@
+package practice.leetcode.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        //int [] nums = {3,0,1};
+        //int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        int[] nums = {0,1};
+        System.out.println("Missing Number  = " + missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int missingNumber = -1;
+        Arrays.sort(nums);
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],1);
+        }
+        int [] reference = new int[nums.length+1];
+        for (int i = 0; i < reference.length; i++) {
+            reference[i]=i;
+        }
+        for (int i = 0; i <reference.length; i++) {
+            if(!map.containsKey(i)){
+                 missingNumber = i;
+                 break;
+            }
+        }
+        return missingNumber;
+    }
+}
