0d0338c797587c03be2300e0b20d253dab5c7987
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-05 16:02:05 +0530

    java array

--- src/main/java/practice/leetcode/arrays/MountainArray.java
+++ src/main/java/practice/leetcode/arrays/MountainArray.java
@@ -5,27 +5,39 @@ package practice.leetcode.arrays;
  */
 public class MountainArray {
     public static void main(String[] args) {
-         int [] arr = {0,3,2,1};
+        //int [] arr = {0,3,2,1};
+        //int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         //int [] arr = {1, 2, 2, 3, 2, 1};
+        int [] arr = {9,8,7,6,5,4,3,2,1,0};
         //int [] arr = {3,5,5};
         System.out.println("Output = " + validMountainArray(arr));
 
     }
 
     public static boolean validMountainArray(int[] arr) {
-        boolean isMountainArray = true;
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
-            if (arr[start] < arr[start + 1]) {
-                start++;
-            } else if (arr[end] < arr[end - 1]) {
-                end--;
-            } else {
-                isMountainArray = false;
-                break;
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        return isMountainArray;
+        if (arr.length <= 2 || max == arr[arr.length-1] || max == arr[0]) return false;
+        else {
+            boolean isMountainArray = true;
+            int start = 0;
+            int end = arr.length - 1;
+            System.out.println("max = " + max);
+            while (start < end) {
+                if (arr[start] < arr[start + 1]) {
+                    start++;
+                } else if (arr[end] < arr[end - 1]) {
+                    end--;
+                } else {
+                    isMountainArray = false;
+                    break;
+                }
+            }
+            return isMountainArray;
+        }
     }
 }
