ea22e64db288b670235d68e60bee9d9d2d31a30a
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-10 21:59:02 +0530

    java array

--- src/main/java/practice/leetcode/arrays/CheckIfNAndItsDoubleExists.java
+++ src/main/java/practice/leetcode/arrays/CheckIfNAndItsDoubleExists.java
@@ -4,29 +4,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *  Leetcode : 1346. Check If N and Its Double Exist
- *
+ * Leetcode : 1346. Check If N and Its Double Exist
  */
 public class CheckIfNAndItsDoubleExists {
     public static void main(String[] args) {
         //int [] arr = {10,2,5,3};
         // int [] arr = {3,1,7,11};
-        int [] arr = {0,-2,2};
+        // int [] arr = {0,-2,2};
+        int[] arr = {0, 0};
         System.out.println("checkIfExist(arr) = " + checkIfExist(arr));
     }
+
     public static boolean checkIfExist(int[] arr) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         boolean exists = false;
+        int count = 1;
         for (int i = 0; i < arr.length; i++) {
-           map.put(arr[i], 1);
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if(map.containsKey(2*arr[i]) && !(arr[i] == 0)){
-                exists = true;
-                break;
+            System.out.println("arr[i] = " + arr[i]);
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], count + 1);
+            } else {
+                map.put(arr[i], count);
             }
         }
-        return exists;
+        System.out.println("map.entrySet() = " + map.entrySet());
+        if (map.size() == 1 && map.get(0) == 2) return true;
+        else {
+            for (int i = 0; i < arr.length; i++) {
+                int j = 2 * arr[i];
+                if (map.containsKey(j)) {
+                    System.out.println("arr[i] = " + arr[i]);
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
     }
-
 }
