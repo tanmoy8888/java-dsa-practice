80c1df817af56ec66e8ace7bcbf6d2e96af2cbd8
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-10 21:31:43 +0530

    java array

--- src/main/java/practice/leetcode/arrays/CheckIfNAndItsDoubleExists.java
+++ src/main/java/practice/leetcode/arrays/CheckIfNAndItsDoubleExists.java
@@ -1,15 +1,32 @@
 package practice.leetcode.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  Leetcode : 1346. Check If N and Its Double Exist
  *
  */
 public class CheckIfNAndItsDoubleExists {
     public static void main(String[] args) {
-
+        int [] arr = {10,2,5,3};
+        System.out.println("checkIfExist(arr) = " + checkIfExist(arr));
     }
     public static boolean checkIfExist(int[] arr) {
-
+        Map<Integer,Integer> map = new HashMap<>();
+        boolean exists = false;
+        for (int i = 0; i < arr.length; i++) {
+           map.put(arr[i], 1);
+        }
+        System.out.println("map.keySet() = " + map.keySet());
+        for (int i = 0; i < arr.length; i++) {
+            if(map.containsKey(2*arr[i])){
+                System.out.println("arr[i] = " + arr[i]);
+                exists = true;
+                break;
+            }
+        }
+        return exists;
     }
 
 }
