4b99204e2c63f1cbd49c7a562d0be32c9ef75230
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-10 21:19:34 +0530

    java array

---
+++ src/main/java/practice/leetcode/arrays/CheckIfNAndItsDoubleExists.java
@@ -0,0 +1,12 @@
+package practice.leetcode.arrays;
+
+/**
+ *  Leetcode : 1346. Check If N and Its Double Exist
+ *
+ */
+public class CheckIfNAndItsDoubleExists {
+    public static void main(String[] args) {
+
+    }
+
+}
