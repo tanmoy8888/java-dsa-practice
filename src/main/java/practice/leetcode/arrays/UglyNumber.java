b1900db8dba830bd982ba83d621b3cc587a9044a
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-13 13:15:56 +0530

    java array

---
+++ src/main/java/practice/leetcode/arrays/UglyNumber.java
@@ -0,0 +1,17 @@
+package practice.leetcode.arrays;
+
+/**
+ *  Leetcode : 263. Ugly Number
+ *
+ *  Q : An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+ */
+public class UglyNumber {
+    public static void main(String[] args) {
+      int n = 6;
+        System.out.println("isUgly(n) = " + isUgly(n));
+    }
+    public static boolean isUgly(int n) {
+        boolean isUgly = false;
+        if(n )
+    }
+}
