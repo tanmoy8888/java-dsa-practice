af147cc8d0a7c756df871610c55a0097aa2e4193
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-16 22:28:10 +0530

    java array

--- src/main/java/practice/leetcode/arrays/prefix_sum/RunningSumOf1DArray.java
+++ src/main/java/practice/leetcode/arrays/prefix_sum/RunningSumOf1DArray.java
@@ -1,7 +1,12 @@
 package practice.leetcode.arrays.prefix_sum;
 
+/**
+ *  Leetcode : 1480. Running Sum of 1d Array
+ *
+ *  
+ */
 public class RunningSumOf1DArray {
     public static void main(String[] args) {
-        
+
     }
 }
