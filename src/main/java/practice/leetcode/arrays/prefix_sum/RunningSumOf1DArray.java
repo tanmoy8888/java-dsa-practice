7db8571b918dc64180d595d2918d44813e98e889
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-16 22:30:51 +0530

    java array

--- src/main/java/practice/leetcode/arrays/prefix_sum/RunningSumOf1DArray.java
+++ src/main/java/practice/leetcode/arrays/prefix_sum/RunningSumOf1DArray.java
@@ -1,12 +1,23 @@
 package practice.leetcode.arrays.prefix_sum;
 
+import java.util.Arrays;
+
 /**
  *  Leetcode : 1480. Running Sum of 1d Array
  *
- *  
+ *
  */
 public class RunningSumOf1DArray {
     public static void main(String[] args) {
+     int [] nums = {1,2,3,4};
+        System.out.println("Input = " + Arrays.toString(nums));
+        System.out.println("Output = " + Arrays.toString(runningSum(nums)));
+    }
 
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        return nums;
     }
 }
