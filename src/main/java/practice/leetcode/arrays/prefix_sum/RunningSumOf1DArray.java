957ba7ddc3764ca5cd53ab127d996f67355fc13e
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-16 22:32:05 +0530

    java array

--- src/main/java/practice/leetcode/arrays/prefix_sum/RunningSumOf1DArray.java
+++ src/main/java/practice/leetcode/arrays/prefix_sum/RunningSumOf1DArray.java
@@ -10,7 +10,8 @@ import java.util.Arrays;
 public class RunningSumOf1DArray {
     public static void main(String[] args) {
      //int [] nums = {1,2,3,4};
-     int [] nums = {1,1,1,1,1};
+     //int [] nums = {1,1,1,1,1};
+     int [] nums = {3,1,2,10,1};
         System.out.println("Input = " + Arrays.toString(nums));
         System.out.println("Output = " + Arrays.toString(runningSum(nums)));
     }
