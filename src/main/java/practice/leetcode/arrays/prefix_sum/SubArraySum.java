d92ab6abb0409b33097bac35b82aa7eeaeaa8dd9
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-17 19:37:06 +0530

    java array

--- src/main/java/practice/leetcode/arrays/prefix_sum/SubArraySum.java
+++ src/main/java/practice/leetcode/arrays/prefix_sum/SubArraySum.java
@@ -1,11 +1,13 @@
 package practice.leetcode.arrays.prefix_sum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SubArraySum {
     public static void main(String[] args) {
         //int[] nums = {1, 2, 3};
-        int[] nums = {1,1,1};
+        int[] nums = {1, 1, 1};
         //int k = 3;
         int k = 2;
         System.out.println("Output = " + subarraySum(nums, k));
@@ -13,18 +15,16 @@ public class SubArraySum {
 
     public static int subarraySum(int[] nums, int k) {
         int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 1; i < nums.length; i++) {
             nums[i] = nums[i - 1] + nums[i];
-            System.out.println("nums[i] = " + nums[i]);
-            if (nums[i] == k){
-                System.out.println("Inside if..."+nums[i]);
-                count++;
-            } else if (nums[i] - nums[i-1] == k){
-                System.out.println("Inside if..."+nums[i-1]+" and "+nums[i]);
-                count++;
-            }
+            map.put(nums[i], 1);
         }
         System.out.println("Arrays.toString(nums) = " + Arrays.toString(nums));
+        System.out.println("map.entrySet() = " + map.entrySet());
+
+
+
         return count;
     }
 }
