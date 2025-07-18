1b97f4f5a66cad06b9e8a928ae783404b1642b40
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-18 20:26:29 +0530

    java array

--- src/main/java/practice/leetcode/arrays/prefix_sum/PivotSumIndex.java
+++ src/main/java/practice/leetcode/arrays/prefix_sum/PivotSumIndex.java
@@ -8,24 +8,24 @@ Leetcode : 724. Find Pivot Index
  */
 public class PivotSumIndex {
     public static void main(String[] args) {
-       int [] nums = {1,7,3,6,5,6};
+        int[] nums = {1, 7, 3, 6, 5, 6};
         System.out.println("Input = " + Arrays.toString(nums));
         System.out.println("Output = " + pivotIndex(nums));
     }
 
     public static int pivotIndex(int[] nums) {
         int pivotIndex = -1;
-     int [] prefixSumLeft = new int[nums.length];
+        int[] prefixSumLeft = new int[nums.length];
         int sum = 0;
-        for (int i = nums.length-1; i >=0  ; i--) {
+        for (int i = nums.length - 1; i >= 0; i--) {
             sum += nums[i];
             prefixSumLeft[i] = sum;
         }
         for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i-1];
+            nums[i] += nums[i - 1];
         }
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == prefixSumLeft[i]){
+            if (nums[i] == prefixSumLeft[i]) {
                 pivotIndex = i;
             }
         }
