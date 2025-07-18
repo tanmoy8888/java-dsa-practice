745dfb9fef66cfb950803463f507cc9bddfb2a03
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-18 19:54:08 +0530

    java array

--- src/main/java/practice/leetcode/arrays/prefix_sum/PivotSumIndex.java
+++ src/main/java/practice/leetcode/arrays/prefix_sum/PivotSumIndex.java
@@ -1,7 +1,15 @@
 package practice.leetcode.arrays.prefix_sum;
 
+/*
+Leetcode : 724. Find Pivot Index
+
+ */
 public class PivotSumIndex {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static int pivotIndex(int[] nums) {
+
     }
 }
