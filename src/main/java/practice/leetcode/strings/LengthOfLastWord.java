7366e5f0315749722243991eede830b74e0b5d25
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-19 14:02:09 +0530

    strings

--- src/main/java/practice/leetcode/strings/LengthOfLastWord.java
+++ src/main/java/practice/leetcode/strings/LengthOfLastWord.java
@@ -3,13 +3,13 @@ package practice.leetcode.strings;
 public class LengthOfLastWord {
     public static void main(String[] args) {
         //String s = "Hello World";
-        String s = "   fly me   to   the moon  ";
+        // String s = "   fly me   to   the moon  ";
+        String s = "luffy is still joyboy";
         System.out.println("Output = " + lengthOfLastWord(s));
     }
 
     public static int lengthOfLastWord(String s) {
         String[] arr = s.trim().split(" ");
-        if (arr.length <= 0) return 1;
-        else return arr[arr.length - 1].length();
+        return arr[arr.length - 1].length();
     }
 }
