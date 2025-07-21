38aca59b50b30ec089363ad10996bcb517dc0513
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-21 20:18:22 +0530

    array

--- src/main/java/practice/leetcode/arrays/MajorityOfElements.java
+++ src/main/java/practice/leetcode/arrays/MajorityOfElements.java
@@ -1,10 +1,21 @@
 package practice.leetcode.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityOfElements {
     public static void main(String[] args) {
 
     }
     public static int majorityElement(int[] nums) {
-      
+      Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if(!map.containsKey(Math.abs(nums[i])){
+               map.put(Math.abs(nums[i]),1);
+            }
+            else{
+                map.put(Math.abs(nums[i]),map.get(Math.abs(nums[i]))+1);
+            }
+        }
     }
 }
