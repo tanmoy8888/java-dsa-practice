d68e526eb3b5acecc0417ab916b69b0141c057a7
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-22 19:32:17 +0530

    array

--- src/main/java/practice/leetcode/arrays/MajorityElementTwo.java
+++ src/main/java/practice/leetcode/arrays/MajorityElementTwo.java
@@ -8,21 +8,20 @@ import java.util.*;
 
 public class MajorityElementTwo {
     public static void main(String[] args) {
-        //int[] nums = {3, 2, 3};
-       // int[] nums = {1};
-        int[] nums = {1,2};
+        int[] nums = {3, 2, 3};
+        // int[] nums = {1};
+        //int[] nums = {1, 2};
         System.out.println("Output = " + majorityElement(nums));
     }
 
     public static List<Integer> majorityElement(int[] nums) {
-        if (nums.length >=2) {
+        if (nums.length <= 2) {
             List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < nums.length; i++) {
-                list.add(nums[i]);
+            for (int num : nums) {
+                list.add(num);
             }
             return list;
-        }
-        else {
+        } else {
             int count = nums.length / 3;
             Map<Integer, Integer> map = new HashMap<Integer, Integer>();
             List<Integer> list = new ArrayList<>();
@@ -32,7 +31,6 @@ public class MajorityElementTwo {
                 } else {
                     map.put(nums[Math.abs(i)], map.get(nums[Math.abs(i)]) + 1);
                     if (map.get(nums[Math.abs(i)]) > count) {
-                        System.out.println("count in if = " + count);
                         list.add(nums[Math.abs(i)]);
                     }
                 }
