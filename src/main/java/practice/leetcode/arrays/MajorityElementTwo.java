41820115010648bfa8197f69460ba649cb4d60aa
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-22 19:29:16 +0530

    array

--- src/main/java/practice/leetcode/arrays/MajorityElementTwo.java
+++ src/main/java/practice/leetcode/arrays/MajorityElementTwo.java
@@ -1,9 +1,6 @@
 package practice.leetcode.arrays;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Leetcode : Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
@@ -11,25 +8,36 @@ import java.util.Map;
 
 public class MajorityElementTwo {
     public static void main(String[] args) {
-        int[] nums = {3, 2, 3};
+        //int[] nums = {3, 2, 3};
+       // int[] nums = {1};
+        int[] nums = {1,2};
         System.out.println("Output = " + majorityElement(nums));
     }
 
     public static List<Integer> majorityElement(int[] nums) {
-        int count = nums.length / 3;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[Math.abs(i)])) {
-                map.put(nums[Math.abs(i)], 1);
-            } else {
-                map.put(nums[Math.abs(i)], map.get(nums[Math.abs(i)]) + 1);
-                if (map.get(nums[Math.abs(i)]) > count) {
-                    list.add(nums[Math.abs(i)]);
+        if (nums.length >=2) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                list.add(nums[i]);
+            }
+            return list;
+        }
+        else {
+            int count = nums.length / 3;
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (!map.containsKey(nums[Math.abs(i)])) {
+                    map.put(nums[Math.abs(i)], 1);
+                } else {
+                    map.put(nums[Math.abs(i)], map.get(nums[Math.abs(i)]) + 1);
+                    if (map.get(nums[Math.abs(i)]) > count) {
+                        System.out.println("count in if = " + count);
+                        list.add(nums[Math.abs(i)]);
+                    }
                 }
             }
+            return list;
         }
-        return list;
     }
-
 }
