0a2acf9779a0f7e1ebadd37cf624c39a349bd839
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-23 19:59:00 +0530

    array

--- src/main/java/practice/leetcode/strings/ReverseAString.java
+++ src/main/java/practice/leetcode/strings/ReverseAString.java
@@ -1,14 +1,28 @@
 package practice.leetcode.strings;
 
+import java.util.Arrays;
+
 /**
- *  Leetcode : 344. Reverse String
+ * Leetcode : 344. Reverse String
  */
 
 public class ReverseAString {
     public static void main(String[] args) {
-
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
     }
-    public static void reverseString(char[] s) {
 
+    public static void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        char temp = ' ';
+        while (start < end) {
+            temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println("Output = " + Arrays.toString(s));
     }
 }
