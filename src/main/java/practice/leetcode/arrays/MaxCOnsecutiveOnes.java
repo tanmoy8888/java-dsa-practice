561a2a49efbde653d01d6c93887d8ca0d0c2b7f5
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-24 20:14:52 +0530

    array

--- src/main/java/practice/leetcode/arrays/MaxCOnsecutiveOnes.java
+++ src/main/java/practice/leetcode/arrays/MaxCOnsecutiveOnes.java
@@ -3,8 +3,22 @@ package practice.leetcode.arrays;
 public class MaxCOnsecutiveOnes {
     public static void main(String[] args) {
         int [] nums = {1,1,0,1,1,1};
+        System.out.println("Output = " + findMaxConsecutiveOnes(nums));
     }
     public static int findMaxConsecutiveOnes(int[] nums) {
-
+    int maxCount = 0;
+    int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1){
+                count++;
+            }else {
+                if(count != 0){
+                    if(count > maxCount){
+                        maxCount = count;
+                    }
+                }
+            }
+        }
+        return maxCount;
     }
 }
