0144e74d277ce147dd44df185e4d6861e33be711
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-25 22:28:21 +0530

    array

--- src/main/java/practice/leetcode/arrays/ProductOfArrayWithoutSelf.java
+++ src/main/java/practice/leetcode/arrays/ProductOfArrayWithoutSelf.java
@@ -3,30 +3,41 @@ package practice.leetcode.arrays;
 import java.util.Arrays;
 
 /**
- *
  * Leetcode : 238. Product of Array Except Self
  */
 
 public class ProductOfArrayWithoutSelf {
     public static void main(String[] args) {
-     // int [] nums ={1,2,3,4};
-      int [] nums ={-1,1,0,-3,3};
-      System.out.println("Output = " + Arrays.toString(productExceptSelf(nums)));
+        int[] nums = {1, 2, 3, 4};
+        // int [] nums ={-1,1,0,-3,3};
+        System.out.println("Output = " + Arrays.toString(productExceptSelf(nums)));
     }
+
     public static int[] productExceptSelf(int[] nums) {
         System.out.println("Arrays.toString(Input) = " + Arrays.toString(nums));
-        int [] prefixProduct = new int[nums.length];
+        int[] prefixProduct = new int[nums.length];
         prefixProduct[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            prefixProduct[i] = prefixProduct[i-1] * nums[i];
+            prefixProduct[i] = prefixProduct[i - 1] * nums[i];
         }
         System.out.println("Arrays.toString(prefixProduct) = " + Arrays.toString(prefixProduct));
-        int [] suffixProduct = new int[nums.length];
-        suffixProduct[nums.length-1] = nums[nums.length-1];
-        for (int i = nums.length-2; i >= 0; i--) {
-            suffixProduct[i] = nums[i] * suffixProduct[i+1];
+        int[] suffixProduct = new int[nums.length];
+        suffixProduct[nums.length - 1] = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffixProduct[i] = nums[i] * suffixProduct[i + 1];
         }
         System.out.println("Arrays.toString(suffixProduct) = " + Arrays.toString(suffixProduct));
-        return prefixProduct;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                nums[i] = suffixProduct[i + 1];
+
+            } else {
+                nums[i] = prefixProduct[i - 1] * suffixProduct[i + 1];
+            }
+            System.out.println("i = " + i );
+            System.out.println("nums[i] = " + nums[i]);
+        }
+
+        return nums;
     }
 }
