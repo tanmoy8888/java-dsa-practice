c91b1e14d712d08c0940f9aca758adc74722bf13
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-25 22:07:33 +0530

    array

--- src/main/java/practice/leetcode/arrays/ProductOfArrayWithoutSelf.java
+++ src/main/java/practice/leetcode/arrays/ProductOfArrayWithoutSelf.java
@@ -10,16 +10,23 @@ import java.util.Arrays;
 public class ProductOfArrayWithoutSelf {
     public static void main(String[] args) {
       int [] nums ={1,2,3,4};
-        System.out.println("Output = " + Arrays.toString(productExceptSelf(nums)));
+     // int [] nums ={-1,1,0,-3,3};
+      System.out.println("Output = " + Arrays.toString(productExceptSelf(nums)));
     }
     public static int[] productExceptSelf(int[] nums) {
-    int product = 1;
-        for (int i = 0; i < nums.length; i++) {
-            product *=nums[i];
+        System.out.println("Arrays.toString(Input) = " + Arrays.toString(nums));
+        int [] prefixProduct = new int[nums.length];
+        prefixProduct[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixProduct[i] = prefixProduct[i-1] * nums[i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = product/nums[i];
+        System.out.println("Arrays.toString(prefixProduct) = " + Arrays.toString(prefixProduct));
+        int [] suffixProduct = new int[nums.length];
+        suffixProduct[nums.length-1] = nums[nums.length-1];
+        for (int i = nums.length-2; i > 0; i--) {
+            suffixProduct[i] = nums[i-1] * suffixProduct[i];
         }
-        return nums;
+        System.out.println("Arrays.toString(prefixProduct) = " + Arrays.toString(suffixProduct));
+        return prefixProduct;
     }
 }
